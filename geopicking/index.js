e371a467ae9448d531079f80f5b27f1a4e2f1d40
cultureDesSauvages <culture.des.sauvages@gmail.com>
2024-06-14 09:58:46 +0200

    Update index.js

--- geopicking/index.js
+++ geopicking/index.js
@@ -1,9 +1,19 @@
+
+try {
+ 
+
+
 const https = require('https');
 https.get('https://trefle.io/api/v1/plants?token=WY4938eNStvfSd3tTTUxQNQvoOVBhuaR4RPGbm61R8A', (resp)  => { 
   let data = ''; 
   // A chunk of data has been recieved.
   resp.on('data', (chunk) => {    data += chunk;  }); 
   // The whole response has been received. Print out the result. 
-  resp.on('end', () => {  console.log(JSON.parse(data));  });
-}).on("error", (err) => {  console.log("Error: " + err.message);});
+  resp.on('end', () => {  console.log(JSON.parse(data));  });}).on("error", (err) => {  console.log("Error: " + err.message);});
+
+  } catch (error) {
+  console.error(error);
+  // Expected output: ReferenceError: nonExistentFunction is not defined
+  // (Note: the exact output may be browser-dependent)
+}
 
