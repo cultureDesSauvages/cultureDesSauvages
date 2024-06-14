b543a58265fb261b79dd81b86ca0d4589eee1b6e
cultureDesSauvages <culture.des.sauvages@gmail.com>
2024-06-14 09:54:03 +0200

    Create index.js

---
+++ geopicking/index.js
@@ -0,0 +1,9 @@
+const https = require('https');
+https.get('https://trefle.io/api/v1/plants?token=WY4938eNStvfSd3tTTUxQNQvoOVBhuaR4RPGbm61R8A', (resp)  => { 
+  let data = ''; 
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {    data += chunk;  }); 
+  // The whole response has been received. Print out the result. 
+  resp.on('end', () => {  console.log(JSON.parse(data));  });
+}).on("error", (err) => {  console.log("Error: " + err.message);});
+
