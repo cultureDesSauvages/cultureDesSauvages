150088401ba029dd29c4d2dcf0d93bd605593b5b
cultureDesSauvages <culture.des.sauvages@gmail.com>
2024-06-14 09:29:48 +0200

    Update index.php

--- geopicking/index.php
+++ geopicking/index.php
@@ -7,6 +7,17 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 //curl_setopt( $curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 //curl_setopt($curl, CURLOPT_POSTFIELDS, $encodedData);
 $result = curl_exec($curl);
+
+
+if($result === false)
+{
+    echo 'Curl error: ' . curl_error($curl);
+}
+else
+{
+    echo 'Operation completed without any errors';
+}
+
 curl_close($curl);
 print $result;
 ?>
