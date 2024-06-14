767aa51165281af0bf16f1e76b96863abfaae524
cultureDesSauvages <culture.des.sauvages@gmail.com>
2024-06-14 09:21:10 +0200

    Update index.php

--- geopicking/index.php
+++ geopicking/index.php
@@ -1,17 +1,11 @@
 <?php
-$url = 'https://trefle.io/api/v1/genus';
-$data = array(
-    "token" => "WY4938eNStvfSd3tTTUxQNQvoOVBhuaR4RPGbm61R8A"
-    )
-);
-$encodedData = json_encode($data);
+$url = 'https://trefle.io/api/v1/genus?token=WY4938eNStvfSd3tTTUxQNQvoOVBhuaR4RPGbm61R8A';
 $curl = curl_init($url);
-$data_string = urlencode(json_encode($data));
+
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt( $curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $encodedData);
+//curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+//curl_setopt( $curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+//curl_setopt($curl, CURLOPT_POSTFIELDS, $encodedData);
 $result = curl_exec($curl);
 curl_close($curl);
 print $result;
