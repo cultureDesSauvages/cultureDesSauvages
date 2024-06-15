4c959dc5b5e33caa0fcf658d42f15205a786612b
cultureDesSauvages <culture.des.sauvages@gmail.com>
2024-06-15 09:29:17 +0200

    Update index.php

--- geopicking/index.php
+++ geopicking/index.php
@@ -2,6 +2,8 @@
 $url = 'https://api.gbif.org/v1/species/match?name=allium%20ursinum';
 if (isset($_GET['apiUrl'])) {
    $url = $_GET['apiUrl'];
+   echo ' $url : ' .$url;
+   print_r($_GET)
 }
  
 $curl = curl_init($url);
